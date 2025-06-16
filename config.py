4a34d38c02a47c944cb5721b4d3da87ef379657f
Dmitry <94780854+dmitryboris@users.noreply.github.com>
2025-06-16 15:16:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import clickhouse_connect
 
-PCAP_PATH = r'C:\Users\Dima\Downloads\new\part1.pcap'
-ARCHIVE_PATH = r'C:\Users\Dima\Downloads\new\part1.tar.zst'
-RESTORED_PCAP_PATH = r'C:\Users\Dima\Downloads\new\COPY.pcap'
+PCAP_PATH = r'C:\Users\...\pcap.pcap'
+ARCHIVE_PATH = r'C:\Users\...\pcap.tar.zst'
+RESTORED_PCAP_PATH = r'C:\Users\...\copy_pcap.pcap'
 BATCH_SIZE = 5000
 
 client = clickhouse_connect.get_client(
@@ -12,10 +12,3 @@ client = clickhouse_connect.get_client(
     password='',
     database='default',
 )
-# client = Client(
-#     host='localhost',
-#     port=9000,
-#     user='default',
-#     password='',
-#     database='default'
-# )
